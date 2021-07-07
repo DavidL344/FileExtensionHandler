40473a5d9e7b851887837c1f6aa6f02768dd358f
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2021-07-07 12:33:08 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileExtensionHandler
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string[] Args { get; private set; } = new string[] { };
+
+        private void OnStart(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length > 0)
+            {
+                Args = e.Args;
+            }
+        }
+    }
+}
