84d2f198e8541c4d66d986337fe2f36e4c4a721a
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2021-11-22 16:50:33 +0100

    Add tests for opening the file with its path

---
+++ FileExtensionHandler.Core.Tests/FilePathsTest.cs
@@ -0,0 +1,48 @@
+﻿using FileExtensionHandler.Core.Tests.Samples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileExtensionHandler.Core.Tests
+{
+    [TestClass]
+    public class FilePathsTest
+    {
+        [TestMethod]
+        public void GetFileInformation()
+        {
+            foreach (KeyValuePair<string, string> validFilePath in FilePaths.Valid)
+            {
+                FileInformation fileInformation = new FileInformation(validFilePath.Value, Vars.Dir_Associations, Vars.Dir_FileExtensions);
+                Assert.AreEqual(Path.GetExtension(Path.GetExtension(validFilePath.Value)), fileInformation.Data.Extension);
+            }
+        }
+
+        [TestMethod]
+        public void ReadFileAssociations()
+        {
+            foreach (KeyValuePair<string, string> validArgument in FilePaths.Valid)
+            {
+                FileInformation fileInformation = new FileInformation(validArgument.Value, Vars.Dir_Associations, Vars.Dir_FileExtensions);
+                Assert.IsNotNull(fileInformation.Data.AssociationsList);
+                Assert.IsTrue(fileInformation.Data.AssociationsList.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void CatchInvalidFilePaths()
+        {
+            foreach (KeyValuePair<string, string> invalidFilePath in FilePaths.Invalid)
+            {
+                Assert.ThrowsException<ArgumentException>(() => new FileInformation(invalidFilePath.Value, Vars.Dir_Associations, Vars.Dir_FileExtensions));
+            }
+        }
+
+        [TestMethod]
+        public void CatchNotAssociatedFileExtension()
+        {
+            Assert.ThrowsException<FileNotFoundException>(() => new FileInformation(FilePaths.FileWithNoFileExtensionInfo, Vars.Dir_Associations, Vars.Dir_FileExtensions));
+        }
+    }
+}
---
+++ FileExtensionHandler.Core.Tests/Samples/FilePaths.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExtensionHandler.Core.Tests.Samples
+{
+    class FilePaths
+    {
+        private static readonly string BaselinePath = $@"{Vars.Dir_Files}\Sample.mp3";
+        public static readonly Dictionary<string, string> Valid = new Dictionary<string, string>()
+        {
+            { "Disk", BaselinePath },
+            { "DiskFileProtocol", $"file:///{BaselinePath}".Replace('\\', '/') },
+            { "FexthDisk", $"fexth://{BaselinePath}".Replace('\\', '/') },
+            { "FexthDiskFileProtocol", $"fexth://file:///{BaselinePath}".Replace('\\', '/') },
+        };
+        public static readonly Dictionary<string, string> Invalid = new Dictionary<string, string>()
+        {
+            { "Disk_Path", Valid["Disk"].Insert(0, "|") },
+            { "Disk_FileName", Valid["Disk"].Reverse().ToString().Insert(0, "|").Reverse().ToString() },
+
+            { "DiskFileProtocol_Path", $"file:///{BaselinePath}".Replace('\\', '/').Insert(0, "|") },
+            { "DiskFileProtocol_FileName", $"file:///{BaselinePath}".Replace('\\', '/').Reverse().ToString().Insert(0, "|").Reverse().ToString() },
+
+            { "FexthDisk_Path", $"fexth://{BaselinePath}".Replace('\\', '/').Insert(0, "|") },
+            { "FexthDisk_FileName", $"fexth://{BaselinePath}".Replace('\\', '/').Reverse().ToString().Insert(0, "|").Reverse().ToString() },
+
+            { "FexthDiskFileProtocol_Path", $"fexth://file:///{BaselinePath}".Replace('\\', '/').Insert(0, "|") },
+            { "FexthDiskFileProtocol_FileName", $"fexth://file:///{BaselinePath}".Replace('\\', '/').Reverse().ToString().Insert(0, "|").Reverse().ToString() }
+        };
+        public static readonly string FileWithNoFileExtensionInfo = Path.ChangeExtension(BaselinePath, ".unknownFileExtension");
+    }
+}
