589286043b790ad9ed5727f70c9392f6c4bdf974
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2021-12-07 23:38:53 +0100

    Bring back the in-app samples

---
+++ FileExtensionHandler.Core/Samples.cs
@@ -0,0 +1,143 @@
+﻿using FileExtensionHandler.Core.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExtensionHandler.Core
+{
+    public class Samples
+    {
+        public static Dictionary<string, FileExtension> FileExtensions => new Dictionary<string, FileExtension>{
+            {
+                ".mp3", new FileExtension
+                {
+                    Node = ".mp3",
+                    Name = "MP3 Format Sound",
+                    Icon = @"%SystemRoot%\system32\wmploc.dll",
+                    IconIndex = -732,
+                    Associations = new string[]
+                    {
+                        "fexth.foobar2000.play",
+                        "fexth.foobar2000.add",
+                        "fexth.wmplayer.play"
+                    },
+                    DefaultAssociation = null
+                }
+            },
+            {
+                ".flac", new FileExtension
+                {
+                    Node = ".flac",
+                    Name = "FLAC Audio",
+                    Icon = @"%SystemRoot%\system32\wmploc.dll",
+                    IconIndex = -738,
+                    Associations = new string[]
+                    {
+                        "fexth.foobar2000.play",
+                        "fexth.foobar2000.add",
+                        "fexth.wmplayer.play"
+                    },
+                    DefaultAssociation = null
+                }
+            },
+            {
+                ".pdf", new FileExtension
+                {
+                    Node = ".pdf",
+                    Name = "PDF File",
+                    Icon = @"%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe",
+                    IconIndex = 13,
+                    Associations = new string[]
+                    {
+                        "fexth.msedge.open",
+                        "fexth.msedge.open-private"
+                    },
+                    DefaultAssociation = null
+                }
+            }
+        };
+
+        public static Dictionary<string, Association> Associations => new Dictionary<string, Association>()
+        {
+            {
+                "fexth.foobar2000.play", new Association
+                {
+                    Node = "fexth.foobar2000.play",
+                    Name = "Foobar2000 (Play)",
+                    Icon = null,
+                    IconIndex = 0,
+                    Command = @"%ProgramFiles(x86)%\foobar2000\foobar2000.exe",
+                    Arguments = "%1"
+                }
+            },
+            {
+                "fexth.foobar2000.add", new Association
+                {
+                    Node = "fexth.foobar2000.add",
+                    Name = "Foobar2000 (Add to queue)",
+                    Icon = null,
+                    IconIndex = 0,
+                    Command = @"%ProgramFiles(x86)%\foobar2000\foobar2000.exe",
+                    Arguments = "/add %1"
+                }
+            },
+            {
+                "fexth.wmplayer.play", new Association
+                {
+                    Node = "fexth.wmplayer.play",
+                    Name = "Windows Media Player",
+                    Icon = null,
+                    IconIndex = 0,
+                    Command = @"%ProgramFiles(x86)%\Windows Media Player\wmplayer.exe",
+                    Arguments = "/prefetch:6 /Open %1"
+                }
+            },
+            {
+                "fexth.msedge.open", new Association
+                {
+                    Node = "fexth.msedge.open",
+                    Name = "Microsoft Edge",
+                    Icon = null,
+                    IconIndex = 0,
+                    Command = @"%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe",
+                    Arguments = "%1"
+                }
+            },
+            {
+                "fexth.msedge.open-private", new Association
+                {
+                    Node = "fexth.msedge.open-private",
+                    Name = "Microsoft Edge (Private Window)",
+                    Icon = null,
+                    IconIndex = 0,
+                    Command = @"%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe",
+                    Arguments = "-inprivate %1"
+                }
+            }
+        };
+
+        public static void SaveToDisk(Dictionary<string, FileExtension> fileExtensionsList, string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath)) Directory.CreateDirectory(directoryPath);
+            foreach (KeyValuePair<string, FileExtension> entry in fileExtensionsList)
+            {
+                string jsonData = JsonConvert.SerializeObject(entry.Value, Formatting.Indented);
+                File.WriteAllText($@"{directoryPath}\{entry.Key}.json", jsonData);
+            }
+        }
+
+        public static void SaveToDisk(Dictionary<string, Association> associationsList, string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath)) Directory.CreateDirectory(directoryPath);
+            foreach (KeyValuePair<string, Association> entry in associationsList)
+            {
+                string jsonData = JsonConvert.SerializeObject(entry.Value, Formatting.Indented);
+                File.WriteAllText($@"{directoryPath}\{entry.Key}.json", jsonData);
+            }
+        }
+    }
+}
--- FileExtensionHandler.Wpf/Pages/Home.xaml.cs
+++ FileExtensionHandler.Wpf/Pages/Home.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using FileExtensionHandler.Core;
+using FileExtensionHandler.Shared;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +27,51 @@ namespace FileExtensionHandler.Pages
         public Home()
         {
             InitializeComponent();
+            CheckSamples();
+        }
+
+        private void CheckSamples()
+        {
+            bool freshInstallation = false;
+            while (!freshInstallation)
+            {
+                if (!Directory.Exists(Vars.DefaultSaveLocation)) freshInstallation = true;
+                if (!Directory.Exists(Vars.Dir_FileExtensions)) freshInstallation = true;
+                if (!Directory.Exists(Vars.Dir_Associations)) freshInstallation = true;
+
+                if (Directory.Exists(Vars.Dir_FileExtensions))
+                    if (!Directory.EnumerateFiles(Vars.Dir_FileExtensions).Any()) freshInstallation = true;
+
+                if (Directory.Exists(Vars.Dir_Associations))
+                    if (!Directory.EnumerateFiles(Vars.Dir_Associations).Any()) freshInstallation = true;
+                break;
+            }
+
+            if (freshInstallation)
+            {
+                txt_instructions.Text = "Click the button below to generate a few associations for the app:";
+                btn_openAppDir.Visibility = Visibility.Collapsed;
+                btn_newAssociations.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txt_instructions.Text = "Call this app with a parameter to get started.";
+                btn_openAppDir.Visibility = Visibility.Visible;
+                btn_newAssociations.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void SaveSamplesToDisk(object sender, RoutedEventArgs e)
+        {
+            Samples.SaveToDisk(Samples.FileExtensions, Vars.Dir_FileExtensions);
+            Samples.SaveToDisk(Samples.Associations, Vars.Dir_Associations);
+            CheckSamples();
+        }
+
+        private void OpenAppDataDir(object sender, RoutedEventArgs e)
+        {
+            CheckSamples();
+            if (Directory.Exists(Vars.DefaultSaveLocation)) Process.Start(Vars.DefaultSaveLocation);
         }
     }
 }
