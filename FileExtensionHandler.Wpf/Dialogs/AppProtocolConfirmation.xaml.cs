3f78d84be9375298be2a347f92a6cac381287fa1
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2021-12-20 22:12:32 +0100

    Warn the user when executing from the app's protocol

---
+++ FileExtensionHandler.Wpf/Dialogs/AppProtocolConfirmation.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileExtensionHandler.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for AppProtocolConfirmation.xaml
+    /// </summary>
+    public partial class AppProtocolConfirmation
+    {
+        internal bool Continue { get; private set; } = false;
+        public AppProtocolConfirmation()
+        {
+            InitializeComponent();
+        }
+
+        private void Confirm(ModernWpf.Controls.ContentDialog sender, ModernWpf.Controls.ContentDialogButtonClickEventArgs args)
+        {
+            Continue = true;
+        }
+    }
+}
--- FileExtensionHandler.Wpf/Pages/AppPicker.xaml.cs
+++ FileExtensionHandler.Wpf/Pages/AppPicker.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using FileExtensionHandler.Core;
 using FileExtensionHandler.Core.Model;
+using FileExtensionHandler.Dialogs;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,17 +30,33 @@ namespace FileExtensionHandler.Pages
         {
             InitializeComponent();
             this.FileInformation = fileInformation;
+
+            if (fileInformation.CalledFromAppProtocol) FexthProtocolConfirm();
             LoadAssociations();
+
+            if (fileInformation.Associations.Count == 0)
+            {
+                lb_selection.Items.Clear();
+                ListBoxItem listBoxItem = new ListBoxItem
+                {
+                    Content = $"Let Windows decide what to do with the file extension"
+                };
+                lb_selection.Items.Add(listBoxItem);
+                footer.Text = fileInformation.Location;
+            }
         }
 
         private void LoadAssociations()
         {
-            if (FileInformation.FileExtension.Node == null || FileInformation.FileExtension.DefaultCheckboxHide)
-                chk_remember.Visibility = Visibility.Collapsed;
-            List<Association> associationsList = FileInformation.Associations;
-            if (associationsList.Count == 0) return;
+            if (FileInformation.FileExtension == null
+                || FileInformation.FileExtension.Node == null
+                || FileInformation.Associations.Count == 0
+                || FileInformation.FileExtension.DefaultCheckboxHide) chk_remember.Visibility = Visibility.Collapsed;
 
             lb_selection.Items.Clear();
+            List<Association> associationsList = FileInformation.Associations;
+            if (associationsList.Count == 0) return;
+            
             foreach (Association fileAssociation in associationsList)
             {
                 ListBoxItem listBoxItem = new ListBoxItem
@@ -69,13 +88,39 @@ namespace FileExtensionHandler.Pages
             if (Keyboard.IsKeyDown(Key.Escape)) id = -1;
             if (id == -1) return;
 
-            if ((bool)chk_remember.IsChecked)
+            if (chk_remember.IsVisible && (bool)chk_remember.IsChecked)
             {
                 FileInformation.FileExtension.DefaultAssociation = FileInformation.Associations[id].Node;
                 FileInformation.SaveFileExtensionInfo();
             }
-            FileInformation.OpenWith(id);
+
+            if (FileInformation.Associations.Count > 0)
+            {
+                FileInformation.OpenWith(id);
+            }
+            else
+            {
+                try
+                {
+                    Process.Start(FileInformation.Location);
+                }
+                catch (Exception e)
+                {
+                    // Suppress the exception when the user cancels the UAC prompt
+                    int errorCode = (e is Win32Exception) ? (e as Win32Exception).NativeErrorCode : e.HResult;
+                    if (errorCode != 1223) MessageBox.Show(String.Format("An unknown error has occured.\r\nException type: {0}\r\nException Description: {1}", e.GetType(), e.Message));
+                }
+            }
             Application.Current.Shutdown();
+        }
+
+        private async void FexthProtocolConfirm()
+        {
+            grd_main.IsEnabled = false;
+            AppProtocolConfirmation confirmation = new AppProtocolConfirmation();
+            await confirmation.ShowAsync();
+            if (!confirmation.Continue) Application.Current.Shutdown();
+            grd_main.IsEnabled = true;
         }
 
         private void ListBoxLoaded(object sender, RoutedEventArgs e)
