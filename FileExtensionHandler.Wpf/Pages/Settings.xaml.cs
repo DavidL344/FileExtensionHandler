e3701b76148a2b243f566d5bf60115ce7dd576f1
DavidL344 <69001314+DavidL344@users.noreply.github.com>
2021-12-22 17:36:12 +0100

    Move the registry operations to the core

--- FileExtensionHandler.Wpf/Pages/Settings.xaml.cs
+++ FileExtensionHandler.Wpf/Pages/Settings.xaml.cs
@@ -1,11 +1,11 @@
-﻿using FileExtensionHandler.Shared;
+﻿using FileExtensionHandler.Core;
+using FileExtensionHandler.Shared;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,55 +25,7 @@ namespace FileExtensionHandler.Pages
     /// </summary>
     public partial class Settings : Page
     {
-        private static readonly string Protocol = "fexth";
-        private readonly string ProtocolFullPath = $@"{Protocol}\shell\open\command";
-        private bool IsAdministrator => new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
-        private bool IsProtocolRegistered
-        {
-            get
-            {
-                try
-                {
-                    bool isRegistered = true;
-
-                    using (RegistryKey key = Registry.ClassesRoot.OpenSubKey(Protocol))
-                    {
-                        if (key == null)
-                        {
-                            isRegistered = false;
-                        }
-                        else
-                        {
-                            if ((string)key.GetValue("URL Protocol") != "") isRegistered = false;
-                        }
-                    }
-
-                    using (RegistryKey key = Registry.ClassesRoot.OpenSubKey(ProtocolFullPath))
-                    {
-                        if (key == null)
-                        {
-                            isRegistered = false;
-                        }
-                        else
-                        {
-                            string regPath = (string)key.GetValue("");
-                            string[] delimiter = new string[] { " %1" };
-                            string[] quotesDelimiter = new string[] { "\"" };
-                            regPath = regPath.Split(delimiter, StringSplitOptions.RemoveEmptyEntries)[0].Split(quotesDelimiter, StringSplitOptions.RemoveEmptyEntries)[0];
-                            if (regPath != Vars.AssemblyLocation) isRegistered = false;
-                        }
-                    }
-                        
-
-                    return isRegistered;
-                }
-                catch (Exception)
-                {
-                    return false;
-                }
-            }
-        }
-
+        private readonly AppRegistry AppRegistry = new AppRegistry(Vars.AssemblyLocation);
         public Settings()
         {
             InitializeComponent();
@@ -85,12 +37,12 @@ namespace FileExtensionHandler.Pages
             chk_regProtocol.IsEnabled = false;
             try
             {
-                if (!IsProtocolRegistered)
+                if (!AppRegistry.IsProtocolRegistered)
                 {
-                    FexthProtocolAdd();
+                    AppRegistry.RegisterProtocol();
                     return;
                 }
-                FexthProtocolRemove();
+                AppRegistry.UnregisterProtocol();
             }
             catch (Exception ex)
             {
@@ -105,57 +57,9 @@ namespace FileExtensionHandler.Pages
             }
         }
 
-        private void FexthProtocolAdd()
-        {
-            if (IsProtocolRegistered) return;
-            string regCommand = $"\"{Vars.AssemblyLocation}\" %1";
-            string regCommandCmd = $"\\\"{Vars.AssemblyLocation}\\\" %1";
-
-            if (!IsAdministrator)
-            {
-                ProcessStartInfo processStartInfo = new ProcessStartInfo
-                {
-                    FileName = "cmd.exe",
-                    Arguments = $"/c reg add \"HKCR\\{Protocol}\" /v \"URL Protocol\" /t REG_SZ /f" +
-                    $"&&reg add \"HKCR\\{ProtocolFullPath}\" /ve /d \"{regCommandCmd}\" /f",
-                    Verb = "runas",
-                    CreateNoWindow = true,
-                    WindowStyle = ProcessWindowStyle.Hidden
-                };
-                Process process = Process.Start(processStartInfo);
-                process.WaitForExit();
-                return;
-            }
-            RegistryKey fexth_root = Registry.ClassesRoot.CreateSubKey(Protocol);
-            fexth_root.SetValue("URL Protocol", "");
-
-            RegistryKey fexth_command = Registry.ClassesRoot.CreateSubKey(ProtocolFullPath);
-            fexth_command.SetValue("", regCommand);
-        }
-
-        private void FexthProtocolRemove()
-        {
-            if (!IsProtocolRegistered) return;
-            if (!IsAdministrator)
-            {
-                ProcessStartInfo processStartInfo = new ProcessStartInfo
-                {
-                    FileName = "cmd.exe",
-                    Arguments = $"/c reg delete \"HKCR\\{Protocol}\" /f",
-                    Verb = "runas",
-                    CreateNoWindow = true,
-                    WindowStyle = ProcessWindowStyle.Hidden
-                };
-                Process process = Process.Start(processStartInfo);
-                process.WaitForExit();
-                return;
-            }
-            Registry.ClassesRoot.DeleteSubKeyTree(Protocol);
-        }
-
         private void RefreshRegistryCheckBox()
         {
-            chk_regProtocol.IsChecked = IsProtocolRegistered;
+            chk_regProtocol.IsChecked = AppRegistry.IsProtocolRegistered;
             chk_regProtocol.Content = (bool)chk_regProtocol.IsChecked ? "Registered the app's file protocol" : "Register the app's file protocol";
         }
     }
